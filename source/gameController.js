c1643dda229d7d8bd6eba3ab274e4ff47563d5f9
Walocial <walocial@hotmail.com>
2023-10-04 10:02:32 +0200

    Timer function rykket ind i gameController

--- source/gameController.js
+++ source/gameController.js
@@ -111,4 +111,34 @@ function doTilesMatch(tile1, tile2) {
     }
 
 
+// Script for timeren
+const timerh3 = document.getElementById('timer');
+let totalSeconds = 0;
+let isPaused = false;
 
+function printTimer() {
+  if (!isPaused){
+    totalSeconds++;
+  
+    const minutesValue = Math.floor(totalSeconds / 60);
+    const secondsValue = totalSeconds % 60;
+  
+    const preMinutes = minutesValue.toString().padStart(2, '0');
+    const preSeconds = secondsValue.toString().padStart(2, '0');
+    // padStart bruges for at formatterer tallene til at være '01' i stedet for '1'
+  
+    timerh3.textContent = preMinutes + ':' + preSeconds; 
+    document.title = preMinutes + ':' + preSeconds + " - Arkaden Vendespil";
+  }
+}
+let timer = setInterval(printTimer, 1000);
+function startTimer() {timer};
+function resetTimer() {totalSeconds = 0};
+function pauseTimer() {
+  if(!isPaused){
+    isPaused = true
+  } else {
+    isPaused = false;
+  }
+};
+startTimer();
