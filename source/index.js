9c75b5e0ad233049bf58b70553e2aa0f57b21fdd
Walocial <walocial@hotmail.com>
2023-10-04 09:55:43 +0200

    Flere Timer Functions, pause og restart

--- source/index.js
+++ source/index.js
@@ -55,20 +55,31 @@ createBoard();
 // Script for timeren
 const timerh3 = document.getElementById('timer');
 let totalSeconds = 0;
+let isPaused = false;
 
 function printTimer() {
-  totalSeconds++;
-
-  const minutesValue = Math.floor(totalSeconds / 60);
-  const secondsValue = totalSeconds % 60;
-
-  const preMinutes = minutesValue.toString().padStart(2, '0');
-  const preSeconds = secondsValue.toString().padStart(2, '0');
-  // padStart bruges for at formatterer tallene til at være '01' i stedet for '1'
-
-  timerh3.textContent = preMinutes + ':' + preSeconds; 
-  document.title = preMinutes + ':' + preSeconds + " - Arkaden Vendespil";
+  if (!isPaused){
+    totalSeconds++;
+  
+    const minutesValue = Math.floor(totalSeconds / 60);
+    const secondsValue = totalSeconds % 60;
+  
+    const preMinutes = minutesValue.toString().padStart(2, '0');
+    const preSeconds = secondsValue.toString().padStart(2, '0');
+    // padStart bruges for at formatterer tallene til at være '01' i stedet for '1'
+  
+    timerh3.textContent = preMinutes + ':' + preSeconds; 
+    document.title = preMinutes + ':' + preSeconds + " - Arkaden Vendespil";
+  }
 }
-
-function startTimer() {setInterval(printTimer, 1000);}
+let timer = setInterval(printTimer, 1000);
+function startTimer() {timer};
+function resetTimer() {totalSeconds = 0};
+function pauseTimer() {
+  if(!isPaused){
+    isPaused = true
+  } else {
+    isPaused = false;
+  }
+};
 startTimer();
